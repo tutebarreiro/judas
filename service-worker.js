b006ef86b37a918a684b8a0c5644536ea98252ac
Mathias Barreiro <mathiasbarreiro@hotmail.com>
2026-05-06 13:37:31 -0300

    Cache

--- service-worker.js
+++ service-worker.js
@@ -13,7 +13,13 @@ self.addEventListener("install", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request))
   );
 });
